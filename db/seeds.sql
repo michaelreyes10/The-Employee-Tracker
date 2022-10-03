9e706a972552160a810019db1a514af84a8821ce
Michaels-Macbook-Pro <michaelreyesbusiness@gmail.com>
2022-10-03 11:09:25 -0400

    fixing seeds error

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,7 +3,7 @@ VALUES
 
     ('Customer Service'),
     ('Business Operations'),
-    ('Management')
+    ('Management');
 INSERT INTO roles (title, salary, department_id)
 VALUES
 
@@ -11,8 +11,8 @@ VALUES
     ('Front Desk Rep', 25000, 1),
 
     ('Sales Rep', 55000, 2),
-    ('Product Developement Assoc.', 60000, 2),
-    ('Product Strategy Assoc.', 45000, 2),
+    ('Product Developement', 60000, 2),
+    ('Product Strategy', 45000, 2),
     ('Marketing Assoc', 50000, 2),
 
     ('Shipping Manager', 50000, 3),
@@ -20,26 +20,26 @@ VALUES
     ('Product Manager', 70000, 3),
     ('Store Manager', 70000, 3);
 
-INSERT INTO employees (first_name, last_name, roles_id, manager_id)
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES 
-    ('Phil', 'Foden', 6, 8),
-    ('Gary', 'Neville', 6, 8),
-    ('Kyle', 'Walker', 6, 8),
+    ('Phil', 'Foden', 6, 1),
+    ('Gary', 'Neville', 6, 1),
+    ('Kyle', 'Walker', 6, 1),
 
-    ('Kierran', 'Tripper', 5, 9),
-    ('Trent', 'Arnold', 5, 9),
+    ('Kierran', 'Tripper', 5, 1),
+    ('Trent', 'Arnold', 5, 1),
 
-    ('Raheem', 'Streling', 4, 9),
-    ('Harry', 'Kane', 4, 9),
+    ('Raheem', 'Streling', 4, 1),
+    ('Harry', 'Kane', 4, 1),
 
-    ('Harry', 'Maguire', 1, 7),
+    ('Harry', 'Maguire', 1, 1),
 
-    ('David ', 'Degea', 2, 7),
+    ('David ', 'Degea', 2, 1),
 
-    ('Luke', 'Shaw', 7, 10),
+    ('Luke', 'Shaw', 7, 1),
 
-    ('Cristiano', 'Ronaldo', 8, 10),
+    ('Cristiano', 'Ronaldo', 8, 1),
 
-    ('Gary', 'Peyton', 9, 10),
+    ('Gary', 'Peyton', 9, 1),
 
-    ('Chad', 'Boswick', 10, 10);
+    ('Chad', 'Boswick', 10, 1);
